47fbd1917bc233245e70fd843f0ec9f453d7cec3
Ko-nifuramu <kotaro22235963@gmail.com>
2023-06-06 18:39:59 +0900

    fix : refactoring main.js

--- main.js
+++ main.js
@@ -1,220 +1,149 @@
-// classでまとめた方がいい気がする
-//sortを使った方が計算量が少なくなる
+class PSMAnalyzer {
+    constructor(csv_array) {
+        this.csv_array = csv_array;
+    }
 
-// 関数が多くなってきたから、util, srcとかのモジュールに分けたい
-//-> jsの場合どうやるのか不明、調べる
+    analyze(analysis_unit) {
+        let psm_array = [];
+        for (let property_index = 1; property_index < this.csv_array[0].length; property_index++) {
+            let property_array = this.eachPropertyAnalyze(property_index, analysis_unit);
+            psm_array.push(property_array);
+        }
+        return psm_array;
+    }
 
-//let,varの使い分けがうまくできてない気がする
+    eachPropertyAnalyze(property_index, analysis_unit) {
+        let each_property_array = [];
+        for (let i = 1; i < this.csv_array.length; i++) {
+            each_property_array.push(Number(this.csv_array[i][property_index]));
+        }
 
-main();
+        each_property_array.sort((a,b)=> {
+            return a > b ? 1: -1;
+        });
+        let is_high = (property_index === 1 || property_index === 3);
+        let count_array = this.countHelper(each_property_array, is_high, analysis_unit);
+        return count_array;
+    }
 
-function main(){
+    countHelper(array, is_high, unit) {
+        let count_array = [];
+        let threshold = unit;
+        let array_index = 0;
+
+        while (array_index < array.length) {
+            if ((is_high && array[array_index] > threshold) || (!is_high && array[array_index] > threshold)) {
+            count_array.push(is_high ? array_index : array.length - array_index);
+            threshold += unit;
+            } else {
+            array_index++;
+            }
+        }
     
-    let checkButton = document.getElementById("checkButton");
+        count_array.push(is_high ? 36 : 0);
+        return count_array;
+    }
+
+    searchFourPoint(array_ascend, array_descend, unit) {
+        let index = 0;
+
+        while (array_ascend[index] <= array_descend[index]) {
+            index++;
+        }
+
+        let four_point_array = [
+            [unit * index, array_ascend[index - 1]],
+            [unit * (index + 1), array_ascend[index]],
+            [unit * index, array_descend[index - 1]],
+            [unit * (index + 1), array_descend[index]]
+        ];
+
+        return four_point_array;
+    }
+
+    calCrossPoint(point1, point2, point3, point4) {
+        let cross_point = [];
+        // point : [x, y]
     
-    checkButton.addEventListener('click', start_psm);
+        var x = (point3[1]-point1[1])*(point1[0]-point2[0])*(point3[0]-point4[0])+ 
+                point1[0]*(point1[1]-point2[1])*(point3[0]-point4[0]) - 
+                point3[0]*(point3[1]-point4[1])*(point1[0]-point2[0]);
+
+        x = x/((point1[1]-point2[1])*(point3[0]-point4[0]) - (point1[0]-point2[0])*(point3[1]-point4[1]));
+
+        let y = x*(point1[1]-point2[1])/(point1[0]-point2[0]) + point1[1] - point1[0]*(point1[1]-point2[1])/(point1[0]-point2[0]);
+
+        cross_point.push(x);
+        cross_point.push(y);
+        return cross_point;
+    }
+
+    calFourPSMPoint(psm_analized_data, unit) {
+        let surround_point_pme = this.searchFourPoint(psm_analized_data[2], psm_analized_data[1], unit);
+        let pme = this.calCrossPoint(surround_point_pme[0], surround_point_pme[1], surround_point_pme[2], surround_point_pme[3]);
     
+        let surround_point_opp = this.searchFourPoint(psm_analized_data[2], psm_analized_data[3], unit);
+        let opp = this.calCrossPoint(surround_point_opp[0], surround_point_opp[1], surround_point_opp[2], surround_point_opp[3]);
+    
+        let surround_point_idp = this.searchFourPoint(psm_analized_data[0], psm_analized_data[1], unit);
+        let idp = this.calCrossPoint(surround_point_idp[0], surround_point_idp[1], surround_point_idp[2], surround_point_idp[3]);
+    
+        let surround_point_pmc = this.searchFourPoint(psm_analized_data[0], psm_analized_data[3], unit);
+        let pmc = this.calCrossPoint(surround_point_pmc[0], surround_point_pmc[1], surround_point_pmc[2], surround_point_pmc[3]);
+    
+        return [pme, opp, idp, pmc];
+    }
 }
 
+function startPsm() {
 
-function start_psm(){
+    console.log("-----------start analysis------------");
 
-    //8~28 : psmデータを読み込む
-    let csv = new XMLHttpRequest();
-    csv.open("GET", "PSMrawdata.csv", false);
-
+    let csvRequest = new XMLHttpRequest();
+    csvRequest.open("GET", "PSMrawdata.csv", false);
     try {
-        csv.send(null);
-    } catch(err) {
+        csvRequest.send(null);
+    } catch (err) {
         console.log(err);
     }
 
-    //data.shape : (37, 5)
-    let csvArray = [];
-    // 改行ごとに配列化
-    let lines = csv.responseText.split(/\r\n|\n/);
-    // 1行ごとに処理
-    for (let i = 0; i < lines.length; i++) {
-        let cells = lines[i].split(",");
-        if (cells.length != 1) {
-        csvArray.push(cells);
-        }
-    }
-    
+    let csv_response_text = csvRequest.responseText;
+    let csv_array = csv_response_text.split(/\r\n|\n/)
+        .filter(line => line.split(",").length > 1)
+        .map(line => line.split(","));
 
     let unit_element = document.getElementById("unit");
     let analysis_unit = Number(unit_element.value);
 
-    let psm_four_point = psm(analysis_unit, csvArray);
+    let analyzer = new PSMAnalyzer(csv_array);
+    let psm_analized_data = analyzer.analyze(analysis_unit);
+    let psm_four_point = analyzer.calFourPSMPoint(psm_analized_data, analysis_unit);
 
     let pme_msg = document.getElementById("pme");
     let opp_msg = document.getElementById("opp");
     let idp_msg = document.getElementById("idp");
     let pmc_msg = document.getElementById("pmc");
 
-    pme_msg.innerHTML = "最高価格 : " + Math.round(psm_four_point[0][0]) + "円";
-    opp_msg.innerHTML = "理想価格 : " + Math.round(psm_four_point[1][0]) + "円";
-    idp_msg.innerHTML = "妥協価格 : " + Math.round(psm_four_point[2][0]) + "円";
-    pmc_msg.innerHTML = "最低品質保証価格 : " + Math.round(psm_four_point[3][0]) + "円";
+    pme_msg.innerHTML = "最高価格 : " + Math.round(psm_four_point[0][0]);
+    opp_msg.innerHTML = "理想価格 : " + Math.round(psm_four_point[1][0]);
+    idp_msg.innerHTML = "妥協価格 : " + Math.round(psm_four_point[2][0]);
+    pmc_msg.innerHTML = "最低品質保証価格 : " + Math.round(psm_four_point[3][0]);
 
+    console.log(analysis_unit + "円単位で計算")
+    console.log("\n")
+    console.log("最高価格 : " + psm_four_point[0][0]);
+    console.log("理想価格 : " + psm_four_point[1][0]);
+    console.log("妥協価格 : " + psm_four_point[2][0]);
+    console.log("最低品質保証価格 : " + psm_four_point[3][0]);
+    console.log("------------end analysis-------------");
 }
 
-function psm(analysis_unit, csvArray){
-
-    console.log(csvArray[1][2]);
-    var unit = analysis_unit;
-    var psm_analized_data = psmAnalize(csvArray, unit);
-
-    var array_psm_four_point = calFourPSMPoint(psm_analized_data, unit);
-
-    return array_psm_four_point;
-
-    // console.log("point of extensiveness : 最高価格");
-    // console.log(array_psm_four_point[0][0]);
-    // console.log("Optimum Pricing point : 理想価格");
-    // console.log(array_psm_four_point[1][0]);
-    // console.log("Indifferrence Price Point : 妥協価格");
-    // console.log(array_psm_four_point[2][0]);
-    // console.log("Point of Marginal Cheapness : 最低品質保証価格");
-    // console.log(array_psm_four_point[3][0]);
+function main() {
+    const checkButton = document.getElementById("checkButton");
+    checkButton.addEventListener('click', startPsm);
 }
 
-
-/* util function psm analysis*/
-
-function psmAnalize(data, unit){
-    console.log(data[0].length);
-    
-    let psm_array = []//shape : (4, max_threshold / unit)
-
-    for(var index=1; index<data[0].length; index++){
-        var property_array=[];
-        property_array = eachPropertyAnalize(data, index, unit);
-        // console.log(property_array.length)
-        psm_array[index-1] = property_array
-    }
-
-    return psm_array
-}
-
-
-function eachPropertyAnalize(data, property_index, unit){
-    var array=[];
-    var ret_array=[];
-    
-    for(var i=1; i < data.length; i++){
-        array.push(data[i][property_index]);
-        // console.log(data[i][property_index])
-    }
-
-    //昇順に並べる
-    array.sort((a,b)=> {
-        return a > b ? 1: -1;
-    })
-
-    if(property_index==1 || property_index==3){
-        var isHigh = Boolean("true");
-        ret_array = countHelper(array, isHigh, unit);
-    }
-    else if(property_index==2 || property_index==4){
-        var isHigh = Boolean("");
-        ret_array = countHelper(array, isHigh, unit);
-    }
-
-    console.log(ret_array)
-    return ret_array;
-}
-
-function countHelper(array, isHigh, unit){
-    var count_array = [];
-    if(isHigh){
-        var threshold = unit;
-        var array_index=0;
-
-        while(array_index < array.length){
-            if(array[array_index] > threshold){
-                //thresholdよ小さい顧客が何人いるかを計算
-                count_array.push(array_index);
-                threshold += unit;
-            }
-            else{
-                array_index++;
-            }
-        }
-        count_array.push(36);
-    }
-    else{
-        var threshold = unit;
-        var array_index=0;
-        while(array_index < array.length){
-            if(array[array_index] > threshold){
-                //thresholdより大きい顧客が何人いるかを計算
-                count_array.push(array.length - array_index);
-                threshold += unit;
-            }
-            else array_index++;
-        }
-        count_array.push(0);
-    }
-
-    return count_array;
-}
-
-
-
-/* util function calulate four point*/
-
-function searchFourPoint(array_ascend, array_descend, unit){
-    var index = 0;
-    var four_point_array = []
-    while(array_ascend[index] <= array_descend[index]){
-        index++;
-    }
-
-    four_point_array =  [[unit*index, array_ascend[index-1]], [unit*(index+1), array_ascend[index]],
-                        [unit*index, array_descend[index-1]], [unit*(index+1), array_descend[index]]];
-    
-    return four_point_array;
-}
-
-
-function calCrossPoint(point1, point2, point3, point4){
-    // point : [x, y]
-    var point_array = [];
-    var x = (point3[1]-point1[1])*(point1[0]-point2[0])*(point3[0]-point4[0])
-    + point1[0]*(point1[1]-point2[1])*(point3[0]-point4[0]) - point3[0]*(point3[1]-point4[1])*(point1[0]-point2[0]);
-
-    x = x/((point1[1]-point2[1])*(point3[0]-point4[0]) - (point1[0]-point2[0])*(point3[1]-point4[1]));
-
-    var y = x*(point1[1]-point2[1])/(point1[0]-point2[0]) + point1[1] - point1[0]*(point1[1]-point2[1])/(point1[0]-point2[0]);
-
-    point_array.push(x);
-    point_array.push(y);
-
-    return point_array;
-}
-
-function calFourPSMPoint(psm_analized_data, unit){
-    //point of extensiveness : 最高価格
-    var surround_point_pme = searchFourPoint(psm_analized_data[2], psm_analized_data[1], unit);
-    var pme = calCrossPoint(surround_point_pme[0], surround_point_pme[1], surround_point_pme[2], surround_point_pme[3]);
-
-    //Optimum Pricing point : 理想価格
-    var surround_point_opp = searchFourPoint(psm_analized_data[2], psm_analized_data[3], unit);
-    var opp = calCrossPoint(surround_point_opp[0], surround_point_opp[1], surround_point_opp[2], surround_point_opp[3]);
-
-    //Indifferrence Price Point : 妥協価格
-    var surround_point_idp = searchFourPoint(psm_analized_data[0], psm_analized_data[1], unit);
-    var idp = calCrossPoint(surround_point_idp[0], surround_point_idp[1], surround_point_idp[2], surround_point_idp[3]);
-
-    //Point of Marginal Cheapness : 最低品質保証価格
-    var surround_point_pmc = searchFourPoint(psm_analized_data[0], psm_analized_data[3], unit);
-    var pmc= calCrossPoint(surround_point_pmc[0], surround_point_pmc[1], surround_point_pmc[2], surround_point_pmc[3]);
-
-    return [pme, opp, idp, pmc];
-}
-
-
+main();
 
 
 /*
@@ -232,4 +161,6 @@ https://codelikes.com/javascript-sort/
 html側からjsの変数(unit)に入力 : 
 https://www.javadrive.jp/javascript/form/index1.html
 
+jsでのclass : 
+https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Classes
 */
